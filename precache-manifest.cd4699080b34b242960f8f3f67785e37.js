0dfddf7eb37c0df9bd2d964fba2384a2748aac15
Macky <makrand586@gmail.com>
2020-05-08 13:14:12 +0530

    Updates

---
+++ precache-manifest.cd4699080b34b242960f8f3f67785e37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2cea70be6ba539052bad519bdf69e6",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "8ae548cbab3189f97b16",
+    "url": "/myapp/static/css/main.c7719369.chunk.css"
+  },
+  {
+    "revision": "66b72f40688662330d86",
+    "url": "/myapp/static/js/2.fb6af289.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/myapp/static/js/2.fb6af289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ae548cbab3189f97b16",
+    "url": "/myapp/static/js/main.84a5a056.chunk.js"
+  },
+  {
+    "revision": "e9add39a633cc2aea3b6",
+    "url": "/myapp/static/js/runtime-main.4271d9e2.js"
+  },
+  {
+    "revision": "fa1ecb92ef7b29348d069180b73ee13c",
+    "url": "/myapp/static/media/virus.fa1ecb92.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a41be125cbbd17d1cb610475bce8b92.js"
+  "/myapp/precache-manifest.cd4699080b34b242960f8f3f67785e37.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
